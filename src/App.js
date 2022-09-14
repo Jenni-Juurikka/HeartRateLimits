d2320e30dd289a7eb6001a98e7df51f6fce0b105
Jenni Juurikka <c0juje01@students.oamk.fi>
2022-09-14 21:26:03 +0300

    Finished heart rate task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
 
-function App() {
+export default function App() {
+  const [age, setAge] = useState(0);
+  const maxLimit = 220;
+  const [upperRate, setUpperRate] = useState(0);
+  const [lowerRate, setLowerRate] = useState(0);
+
+  function CalculateHeartRate(e) {
+    e.preventDefault();
+    setUpperRate((maxLimit - age) * 0.85);
+    setLowerRate((maxLimit - age) * 0.65);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Heart Rate Limits</h1>
+      <form onSubmit={CalculateHeartRate}>
+        <label>Age:</label>
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)}/>
+
+        <input type="submit" value="Submit"/>
+      </form>
+
+      <div>
+        <h4>Upper limit: {upperRate}</h4>
+        <h4>Lower limit: {lowerRate}</h4>
+      </div>
     </div>
   );
 }
-
-export default App;
